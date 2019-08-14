b60e88ece1ddff3b609e95f6b7bd9f4c4bd5126f
Thomas <thomas.herzog@gepardec.com>
2019-08-14 08:53:55 +0200

    - Added jenkins resources

--- ocp/ocp.sh
+++ ocp/ocp.sh
@@ -5,18 +5,39 @@ cd $(pwd)
 
 mkdir ${OUT_DIR}
 
-function processSwagger {
-    oc process -f swagger.ui.yaml --parameters-file=swagger-ui.properties > ${OUT_DIR}/swagger-ui.json
+function _processSwagger {
+    oc process -f swagger-ui.yaml --parameters-file=swagger-ui.properties > ${OUT_DIR}/swagger-ui.json
+}
+
+function _processJenkins {
+    oc process -f jenkins-bc.yaml --parameters-file=jenkins.properties > ${OUT_DIR}/jenkins-bc.json
+    oc process -f jenkins-slaves.yaml --parameters-file=jenkins.properties > ${OUT_DIR}/jenkins-slaves.json
 }
 
 function createSwagger {
-    processSwagger
+    _processSwagger
     oc create -f ${OUT_DIR}/swagger-ui.json
 }
 
 function deleteSwagger {
-    processSwagger
+    _processSwagger
     oc delete -f ${OUT_DIR}/swagger-ui.json
+}
+
+function createJenkins {
+    _processJenkins
+    oc create -f ${OUT_DIR}/jenkins-bc.json
+    oc create -f ${OUT_DIR}/jenkins-slaves.json
+}
+
+function deleteJenkins {
+    _processJenkins
+    oc delete -f ${OUT_DIR}/jenkins-bc.json
+    oc delete -f ${OUT_DIR}/jenkins-slaves.json
+}
+
+function deleteJenkinsPv {
+    
 }
 
 function createAll {
