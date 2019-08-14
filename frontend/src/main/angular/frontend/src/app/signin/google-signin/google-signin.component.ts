1d51387d3b60443989e295fe70bdca2215870606
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2019-08-14 11:11:41 +0200

    setup frontend project

---
+++ frontend/src/main/angular/frontend/src/app/signin/google-signin/google-signin.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService, GoogleLoginProvider, SocialUser} from "angularx-social-login";
+
+@Component({
+  selector: 'app-google-signin',
+  templateUrl: './google-signin.component.html',
+  styleUrls: ['./google-signin.component.scss']
+})
+export class GoogleSigninComponent implements OnInit {
+
+  private user: SocialUser;
+  private loggedIn: boolean;
+
+  constructor(
+    private authService: AuthService
+  ) {
+  }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+
+  signinWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+}
