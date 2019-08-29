07ee326a222e5aeb814b81b48f6a9f0315c3e286
Philipp Wurm <philipp.wurm@gepardec.com>
2019-08-29 16:08:12 +0200

    added lombock support

--- mega-zep-backend/src/main/java/connector/rest/api/WorkerApi.java
+++ mega-zep-backend/src/main/java/connector/rest/api/WorkerApi.java
@@ -3,6 +3,7 @@ package connector.rest.api;
 import connector.rest.model.GoogleUser;
 import de.provantis.zep.MitarbeiterType;
 import de.provantis.zep.ReadMitarbeiterResponseType;
+import org.apache.http.HttpStatus;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -10,28 +11,28 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/worker")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface WorkerApi {
+
+    @GET
+    @Path("/status")
+    Response status();
+
     @GET
     @Path("/get")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
+
     String hello();
 
     @POST
     @Path("/get")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
     MitarbeiterType get(GoogleUser user);
 
     @POST
     @Path("/getAll")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
     ReadMitarbeiterResponseType getAll(GoogleUser user);
 
     @PUT
     @Path("/update")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
     Response updateWorker(List<MitarbeiterType> employees);
 }
--- mega-zep-backend/src/main/java/connector/rest/impl/WorkerImpl.java
+++ mega-zep-backend/src/main/java/connector/rest/impl/WorkerImpl.java
@@ -1,14 +1,20 @@
 package connector.rest.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import connector.rest.api.WorkerApi;
 import connector.rest.model.GoogleUser;
 import connector.service.api.WorkerService;
 import de.provantis.zep.MitarbeiterType;
 import de.provantis.zep.ReadMitarbeiterResponseType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.http.HttpStatus;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.List;
 
 @ApplicationScoped
@@ -16,6 +22,21 @@ public class WorkerImpl implements WorkerApi {
 
     @Inject
     WorkerService workerService;
+
+    @Override
+    public Response status () {
+        final ServiceStatus serviceStatus = new ServiceStatus(HttpStatus.SC_OK, "OK");
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final StringWriter stringWriter = new StringWriter();
+        try {
+            objectMapper.writeValue(stringWriter, serviceStatus);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return Response.status(HttpStatus.SC_OK).entity(stringWriter.toString()).build();
+    }
 
     @Override
     public String hello() {
@@ -43,5 +64,12 @@ public class WorkerImpl implements WorkerApi {
     @Override
     public Response updateWorker(List<MitarbeiterType> employees) {
         return workerService.updateWorker(employees);
+    }
+
+    @AllArgsConstructor
+    @Data
+    private class ServiceStatus {
+        private final int code;
+        private final String message;
     }
 }
