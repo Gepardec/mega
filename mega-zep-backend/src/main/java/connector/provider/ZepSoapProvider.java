d3280a1c6f4bea5117dfa9a63a0b0200ef67497b
mbrandm <lord12345@gmx.at>
2019-09-20 14:02:18 +0200

    Avoid Admin-Token in public repo -> secret.soaptoken

--- mega-zep-backend/src/main/java/connector/provider/ZepSoapProvider.java
+++ mega-zep-backend/src/main/java/connector/provider/ZepSoapProvider.java
@@ -3,15 +3,43 @@ package connector.provider;
 import de.provantis.zep.RequestHeaderType;
 import de.provantis.zep.ZepSoap;
 import de.provantis.zep.ZepSoapPortType;
+import org.slf4j.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @ApplicationScoped
 public class ZepSoapProvider {
 
-    private final String AUTHORIZATION_TOKEN = "d51e04437e60eff16d0ee752e9b8f232ce82e943fb2759a79a3df92518778400";
+    @Inject
+    private Logger logger;
+
+    private static final String ADMIN_TOKEN_FILE_NAME = "secret.soaptoken";
+
+    private String authorizationToken;
+
+    @PostConstruct
+    private void initAuthorizationToken() {
+        try {
+            authorizationToken = Files.readString(Paths.get(ADMIN_TOKEN_FILE_NAME), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            String msg = "Error occured while reading file: " + ADMIN_TOKEN_FILE_NAME;
+            logger.error(msg);
+            throw new TokenFileReadException(msg);
+        } catch (NullPointerException npe) {
+            String msg = "File " + ADMIN_TOKEN_FILE_NAME + " not found, please put it in mega-zep-backend-resources";
+            logger.error(msg);
+            throw new TokenFileReadException(msg);
+        }
+    }
+
 
     @Produces
     @Dependent
@@ -24,7 +52,7 @@ public class ZepSoapProvider {
     @Dependent
     RequestHeaderType createRequestHeaderType() {
         RequestHeaderType requestHeaderType = new RequestHeaderType();
-        requestHeaderType.setAuthorizationToken(AUTHORIZATION_TOKEN);
+        requestHeaderType.setAuthorizationToken(authorizationToken);
         return requestHeaderType;
     }
 
