cbb831e6eb1bb61382325e5d2b58a130a3861a41
mbrandm <lord12345@gmx.at>
2019-11-14 12:58:09 +0100

    update mail-service

--- mega-zep-backend/src/main/java/com/gepardec/mega/communication/MailSender.java
+++ mega-zep-backend/src/main/java/com/gepardec/mega/communication/MailSender.java
@@ -1,23 +1,19 @@
 package com.gepardec.mega.communication;
 
 
-import com.gepardec.mega.model.google.GoogleUser;
-import io.quarkus.arc.config.ConfigProperties;
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.List;
 
 @ApplicationScoped
-@ConfigProperties()
 public class MailSender {
 
     @Inject
     Mailer mailer;
 
-    public void send(List<GoogleUser> googleUsers, ) {
+    public void send() {
         System.out.println("start sending mail");
 
         Mail mail = Mail.withText("mario.brandmueller@gepardec.com", "My Mail from Quarkus", "He Bua, wos geht?");
