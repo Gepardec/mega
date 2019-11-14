0238a78fe2633e0bf54a377e5d6b32b4ed0cd372
mbrandm <lord12345@gmx.at>
2019-11-14 13:31:30 +0100

    update mail-service

--- mega-zep-backend/src/main/java/com/gepardec/mega/communication/MailDaemon.java
+++ mega-zep-backend/src/main/java/com/gepardec/mega/communication/MailDaemon.java
@@ -1,4 +1,32 @@
 package com.gepardec.mega.communication;
 
+import com.gepardec.mega.zep.service.api.WorkerService;
+import io.quarkus.scheduler.Scheduled;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
 public class MailDaemon {
+
+    @Inject
+    private MailSender mailSender;
+
+    @Inject
+    private WorkerService workerService;
+
+
+
+
+    @Scheduled(every = "5s")
+    void sendMailToEmployees() {
+        System.out.println("5 seconds past");
+
+        workerService.getAllEmployees()
+
+    }
+
+
+
+
 }
