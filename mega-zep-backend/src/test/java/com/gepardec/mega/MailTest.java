4551274e10add3f860d2639a8fb28b813337040c
mbrandm <lord12345@gmx.at>
2019-11-14 13:00:33 +0100

    update mail-service

--- mega-zep-backend/src/test/java/com/gepardec/mega/MailTest.java
+++ mega-zep-backend/src/test/java/com/gepardec/mega/MailTest.java
@@ -2,11 +2,13 @@ package com.gepardec.mega;
 
 import com.gepardec.mega.communication.MailSender;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
 @QuarkusTest
+@Disabled
 public class MailTest {
 
     @Inject
