899972c0a02325e8685890e5712a80593e07bff3
Philipp Wurm <philipp.wurm@gepardec.com>
2019-11-29 13:04:28 +0100

    set image to mail

--- mega-zep-backend/src/test/java/com/gepardec/mega/MailSenderTest.java
+++ mega-zep-backend/src/test/java/com/gepardec/mega/MailSenderTest.java
@@ -4,7 +4,9 @@ import com.gepardec.mega.communication.MailSender;
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.MockMailbox;
 import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
@@ -13,9 +15,14 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
+@Disabled
+//FIXME set system property surefire
 class MailSenderTest {
 
-    private static final String TO = "megagepardec@gmail.com";
+    private static final String TO = "service@gepardec.com";
+
+    @ConfigProperty(name = "quarkus.mailer.mock")
+    boolean mailMockSetting;
 
     @Inject
     MailSender mailSender;
@@ -30,6 +37,8 @@ class MailSenderTest {
 
     @Test
     void sendMail() {
+        assertTrue(mailMockSetting);
+
         mailSender.sendMonthlyFriendlyReminder(TO, "Jamal");
         List<Mail> sent = mailbox.getMessagesSentTo(TO);
         assertAll(
