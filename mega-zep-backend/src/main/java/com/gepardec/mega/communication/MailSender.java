3ccab63d459c2c0aa83b927e1e70b797d6300b90
Philipp Wurm <philipp.wurm@gepardec.com>
2019-11-29 11:30:47 +0100

    changed mail service settings

--- mega-zep-backend/src/main/java/com/gepardec/mega/communication/MailSender.java
+++ mega-zep-backend/src/main/java/com/gepardec/mega/communication/MailSender.java
@@ -1,31 +1,31 @@
 package com.gepardec.mega.communication;
 
-
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static com.gepardec.mega.communication.Reminder.USER_CHECK_PROJECTTIMES;
 
 @ApplicationScoped
 public class MailSender {
 
-    private static final String REMINDER_1_ST_BUSINESS_DAY_HTML = "reminder1stBusinessDay.html";
+    @ConfigProperty(name = "mega.mail.firstbusinessday.reminder")
+    String MEGA_REMINDER_1_ST_BUSINESS_DAY_CONTENT;
+
+    @ConfigProperty(name = "mega.image.logo.url")
+    String MEGA_IMAGE_LOGO_URL;
+
+    @ConfigProperty(name = "mega.wiki.eom.url")
+    String MEGA_WIKI_EOM_URL;
+
     private static final String NAME_PLACEHOLDER = "$firstName$";
-    private static final String LOGO_PATH = "src/main/resources/LogoMegaDash.png";
+    private static final String EOM_WIKI_PLACEHOLDER = "$wiki_eom$";
+    private static final String LOGO_PLACEHOLDER = "$mega_logo$";
 
     private String mailBody = null;
 
@@ -35,26 +35,13 @@ public class MailSender {
     @Inject
     Logger logger;
 
-    @PostConstruct
-    void readFiles() {
-        try {
-            final Path path = Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource(REMINDER_1_ST_BUSINESS_DAY_HTML)).toURI());
-            try (Stream<String> lines = Files.lines(path)) {
-                mailBody = lines.collect(Collectors.joining(System.lineSeparator()));
-            }
-        } catch (URISyntaxException | IOException e) {
-            logger.error("Error reading message Text from File {} ", REMINDER_1_ST_BUSINESS_DAY_HTML);
-        }
-    }
-
     public void sendMonthlyFriendlyReminder(String eMail, String firstName) {
         String mailContent = Objects.requireNonNull(mailBody)
-                .replace(NAME_PLACEHOLDER, firstName);
+                .replace(NAME_PLACEHOLDER, firstName)
+                .replace(EOM_WIKI_PLACEHOLDER, MEGA_WIKI_EOM_URL)
+                .replace(LOGO_PLACEHOLDER, MEGA_IMAGE_LOGO_URL);
 
-        mailer.send(Mail
-                .withHtml(eMail, USER_CHECK_PROJECTTIMES.getText(), mailContent)
-                .addInlineAttachment("LogoMegaDash.png", new File(LOGO_PATH),
-                        "image/png", "<LogoMegaDash@gepardec.com>"));
+        mailer.send(Mail.withHtml(eMail, USER_CHECK_PROJECTTIMES.getText(), mailContent));
     }
 
     public void sendMail(String eMail, Reminder reminder, String text) {
