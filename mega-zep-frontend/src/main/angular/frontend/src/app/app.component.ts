f3efc6e5743d125528003433d894ce6e25655958
Rainer Neunteufel <rainer.neunteufel@gepardec.com>
2020-03-06 12:38:55 +0100

    update angular-oauth2-oidc to angular-oauth2-oidc 9

--- mega-zep-frontend/src/main/angular/frontend/src/app/app.component.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { OAuthService } from 'angular-oauth2-oidc';
 import { authConfig } from './auth/auth.config';
 import { Router } from '@angular/router';
 import { UserService } from './modules/shared/services/user/user.service';
 import { ConfigService } from './modules/shared/services/config/config.service';
 import { Config } from './modules/shared/models/Config';
 import { Subscription } from 'rxjs';
+import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
 
 @Component({
   selector: 'app-root',
