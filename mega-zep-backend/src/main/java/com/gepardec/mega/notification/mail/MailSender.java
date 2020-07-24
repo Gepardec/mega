c53e2e7fce698ff6d196c1aaf5215da4b82d295f
cchet <thomas.herzog@gepardec.com>
2020-07-24 09:30:13 +0200

    - Fixed refactoring bug

--- mega-zep-backend/src/main/java/com/gepardec/mega/notification/mail/MailSender.java
+++ mega-zep-backend/src/main/java/com/gepardec/mega/notification/mail/MailSender.java
@@ -3,61 +3,24 @@ package com.gepardec.mega.notification.mail;
 import com.google.common.net.MediaType;
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
-import org.slf4j.Logger;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Objects;
-import java.util.Optional;
 
 @ApplicationScoped
 public class MailSender {
-    private static final String NAME_PLACEHOLDER = "$firstName$";
-    private static final String EOM_WIKI_PLACEHOLDER = "$wikiEomUrl$";
-    private static final String MEGA_DASH_URL_PLACEHOLDER = "$megaDash$";
-    private static final String TEMPLATE_MAILTEXT_PLACEHOLDER = "$mailText$";
-
-    private byte[] logoByteArray;
-
-    @ConfigProperty(name = "mega.mail.subjectPrefix")
-    Optional<String> subjectPrefix;
-
-    @ConfigProperty(name = "mega.image.logo.url")
-    String megaImageLogoUrl;
-
-    @ConfigProperty(name = "mega.wiki.eom.url")
-    String megaWikiEomUrl;
-
-    @ConfigProperty(name = "mega.dash.url")
-    String megaDashUrl;
-
-    @ConfigProperty(name = "mega.mail.reminder.template.path")
-    String mailTemplateTextPath;
 
     @Inject
     NotificationConfig notificationConfig;
 
     @Inject
     Mailer mailer;
-
-    @Inject
-    Logger logger;
-
-    private String mailTemplateText;
-
-    @PostConstruct
-    void initLogoAndTemplate() {
-        mailTemplateText = readTextOfPath(mailTemplateTextPath)
-                .replace(MEGA_DASH_URL_PLACEHOLDER, megaDashUrl);
-    }
-
 
     public void sendReminder(String eMail, String firstName, Reminder reminder) {
         String subject = notificationConfig.subjectForReminder(reminder.name());
@@ -67,10 +30,12 @@ public class MailSender {
 
 
     private void sendMail(String eMail, String firstName, String subject, String text) {
-        String mailContent = mailTemplateText
-                .replace(NAME_PLACEHOLDER, firstName)
-                .replace(TEMPLATE_MAILTEXT_PLACEHOLDER, text)
-                .replace(EOM_WIKI_PLACEHOLDER, megaWikiEomUrl);
+        final String mailTemplateText = readTextOfPath(notificationConfig.getMailTemplateTextPath())
+                .replace(notificationConfig.getMegaDashUrlPlaceholder(), notificationConfig.getMegaDashUrl());
+        final String mailContent = mailTemplateText
+                .replace(notificationConfig.getNamePlaceholder(), firstName)
+                .replace(notificationConfig.getTemplateMailtextPlaceholder(), text)
+                .replace(notificationConfig.getEomWikiPlaceholder(), notificationConfig.getMegaWikiEomUrl());
 
 
         mailer.send(Mail.withHtml(eMail, subject, mailContent)
@@ -90,14 +55,15 @@ public class MailSender {
     }
 
     private byte[] readLogo() {
-        final URL url = MailSender.class.getClassLoader().getResource(megaImageLogoUrl);
-        Objects.requireNonNull(url, String.format("File '%s' could not be loaded as a resource", megaImageLogoUrl));
+        final String imageLogoUrl = notificationConfig.getMegaImageLogoUrl();
+        final URL url = MailSender.class.getClassLoader().getResource(imageLogoUrl);
+        Objects.requireNonNull(url, String.format("File '%s' could not be loaded as a resource", imageLogoUrl));
         try {
             return Files.readAllBytes(Paths.get(url.toURI()));
         } catch (IOException e) {
-            throw new IllegalStateException(String.format("Could not read bytes of resource '%s'", megaImageLogoUrl));
+            throw new IllegalStateException(String.format("Could not read bytes of resource '%s'", imageLogoUrl));
         } catch (URISyntaxException e) {
-            throw new IllegalArgumentException(String.format("Could not get URI of found resource '%s'", megaImageLogoUrl));
+            throw new IllegalArgumentException(String.format("Could not get URI of found resource '%s'", imageLogoUrl));
         }
     }
 }
