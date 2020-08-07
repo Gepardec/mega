bd9176cc1286a0c555f881e28f7a8e8adf661040
Rainer Neunteufel <rainer.neunteufel@gepardec.com>
2020-08-07 14:53:38 +0200

    fix build bug

--- mega-zep-backend/src/test/java/com/gepardec/mega/UserContextMock.java
+++ mega-zep-backend/src/test/java/com/gepardec/mega/UserContextMock.java
@@ -4,7 +4,6 @@ import com.gepardec.mega.application.security.Role;
 import com.gepardec.mega.application.security.UserContext;
 import com.gepardec.mega.domain.model.Employee;
 import com.gepardec.mega.domain.model.User;
-import com.gepardec.mega.util.EmployeeTestUtil;
 import io.quarkus.test.Mock;
 
 @Mock
@@ -17,7 +16,19 @@ public class UserContextMock implements UserContext {
 
     @Override
     public User getUser() {
-        final Employee employee = EmployeeTestUtil.createEmployee(0);
+        final String name = "Thomas_0";
+        final Employee employee = Employee.builder()
+                .email(name + "@gepardec.com")
+                .firstName(name)
+                .sureName(name + "_Nachname")
+                .title("Ing.")
+                .userId(String.valueOf(0))
+                .salutation("Herr")
+                .workDescription("ARCHITEKT")
+                .releaseDate("2020-01-01")
+                .role(Role.USER.roleId)
+                .active(true)
+                .build();
         return User.builder()
                 .userId(employee.userId())
                 .email(employee.email())
