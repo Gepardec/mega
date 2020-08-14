9c24a2573e61df062a98c0cc67f6dd0ca2035c95
Rainer Neunteufel <rainer.neunteufel@gepardec.com>
2020-08-14 12:35:23 +0200

    Revert "fixed page refresh issue"
    This reverts commit d2fcfe870c5288c05124fe9c818b580786a30d62.

--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/shared/interceptors/authorization-header.interceptor.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/shared/interceptors/authorization-header.interceptor.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { OAuthStorage } from 'angular-oauth2-oidc';
-import { ConfigService } from '../services/config/config.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +9,11 @@ import { ConfigService } from '../services/config/config.service';
 export class AuthorizationHeaderInterceptor implements HttpInterceptor {
 
   constructor(
-    private configService: ConfigService,
     private authStorage: OAuthStorage) {
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (request.url.startsWith(this.configService.getBackendUrl()) && this.authStorage.getItem('id_token')) {
+    if (this.authStorage.getItem('id_token')) {
       const headers = request.headers.set('X-Authorization', this.authStorage.getItem('id_token'));
       return next.handle(request.clone({headers}));
     } else {
