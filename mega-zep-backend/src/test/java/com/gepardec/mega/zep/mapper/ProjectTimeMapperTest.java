c9b44817d266921a3c7e1490303516081489c936
cchet <thomas.herzog@gepardec.com>
2020-09-11 17:58:53 +0200

    - Some little changes on the test method names - Added a test method which tests for properly mapped states - Made the mapper a cDI Bean - Modified existing code to inject the mapper and modified existing test classes which need to set the mapper via the  constructor

--- mega-zep-backend/src/test/java/com/gepardec/mega/zep/mapper/ProjectTimeMapperTest.java
+++ mega-zep-backend/src/test/java/com/gepardec/mega/zep/mapper/ProjectTimeMapperTest.java
@@ -4,51 +4,95 @@ import com.gepardec.mega.domain.model.monthlyreport.JourneyTimeEntry;
 import com.gepardec.mega.domain.model.monthlyreport.ProjectEntry;
 import com.gepardec.mega.domain.model.monthlyreport.ProjectTimeEntry;
 import de.provantis.zep.ProjektzeitType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProjectTimeMapperTest {
 
-    @Test
-    void mapSingleTypeToEntry_taetigkeitNotEnumType_ThrowsException() {
-        ProjektzeitType projektzeitType = createDefaultProjektzeitType();
-        projektzeitType.setTaetigkeit("testen");
+    private ProjectTimeMapper mapper;
 
-        assertThrows(IllegalArgumentException.class, () -> ProjectTimeMapper.mapToEntryList(List.of(projektzeitType)));
+    @BeforeEach
+    void beforeEach() {
+        mapper = new ProjectTimeMapper();
     }
 
     @Test
-    void mapSingleTypeToEntry_taetigkeitEqualsReiseAndReiseRichtungNotEnumType_ThrowsException() {
+    void mapSingleTypeToEntry_twhenTaetigkeitNotEnumType_thenThrowsException() {
+        ProjektzeitType projektzeitType = createDefaultProjektzeitType();
+        projektzeitType.setTaetigkeit("testen");
+
+        assertThrows(IllegalArgumentException.class, () -> mapper.mapToEntryList(List.of(projektzeitType)));
+    }
+
+    @Test
+    void mapSingleTypeToEntry_whenTaetigkeitEqualsReiseAndReiseRichtungNotEnumType_thenThrowsException() {
         ProjektzeitType projektzeitType = createDefaultProjektzeitType();
         projektzeitType.setTaetigkeit("reisen");
         projektzeitType.setReiseRichtung("100");
 
-        assertThrows(IllegalArgumentException.class, () -> ProjectTimeMapper.mapToEntryList(List.of(projektzeitType)));
+        assertThrows(IllegalArgumentException.class, () -> mapper.mapToEntryList(List.of(projektzeitType)));
     }
 
     @Test
-    void mapSingleTypeToEntry_taetigkeitEqualsReiseAndReiseRichtungEqualsTo_ReturnsProjectTimeEntry() {
+    void mapSingleTypeToEntry_whenTaetigkeitEqualsReiseAndReiseRichtungIsTO_thenReturnsProjectTimeEntryInstance() {
         ProjektzeitType projektzeitType = createDefaultProjektzeitType();
 
-        List<ProjectEntry> result = ProjectTimeMapper.mapToEntryList(List.of(projektzeitType));
+        List<ProjectEntry> result = mapper.mapToEntryList(List.of(projektzeitType));
 
         assertEquals(1, result.size());
         assertTrue(result.get(0) instanceof ProjectTimeEntry);
     }
 
     @Test
-    void mapSingleTypeToEntry_taetigkeitEqualsReiseAndReiseRichtungEqualsTo_ReturnsJourneyTimeEntry() {
+    void mapSingleTypeToEntry_whenTaetigkeitEqualsReiseAndReiseRichtungIsTO_thenReturnsMappedProjectTimeEntry() {
+        ProjektzeitType projektzeitType = createDefaultProjektzeitType();
+        final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm");
+
+        List<ProjectEntry> result = mapper.mapToEntryList(List.of(projektzeitType));
+
+        assertEquals(1, result.size());
+        final ProjectTimeEntry projectTimeEntry = (ProjectTimeEntry) result.get(0);
+        assertEquals(projektzeitType.getDatum(), projectTimeEntry.getDate().format(dateFormatter));
+        assertEquals(projektzeitType.getVon(), projectTimeEntry.getFromTime().format(timeFormatter));
+        assertEquals(projektzeitType.getBis(), projectTimeEntry.getToTime().format(timeFormatter));
+        assertEquals(projektzeitType.getTaetigkeit().toUpperCase(), projectTimeEntry.getTask().name());
+    }
+
+    @Test
+    void mapSingleTypeToEntry_whenTaetigkeitEqualsReiseAndReiseRichtungIsTo_thenReturnsJourneyTimeEntryInstance() {
         ProjektzeitType projektzeitType = createDefaultProjektzeitType();
         projektzeitType.setTaetigkeit("reisen");
         projektzeitType.setReiseRichtung("0");  // JourneyDirection.TO
 
-        List<ProjectEntry> result = ProjectTimeMapper.mapToEntryList(List.of(projektzeitType));
+        List<ProjectEntry> result = mapper.mapToEntryList(List.of(projektzeitType));
 
         assertEquals(1, result.size());
         assertTrue(result.get(0) instanceof JourneyTimeEntry);
+    }
+
+    @Test
+    void mapSingleTypeToEntry_whenTaetigkeitIsReiseAndReiseRichtungIsTO_thenReturnsMappedJourneyTimeEntry() {
+        ProjektzeitType projektzeitType = createDefaultProjektzeitType();
+        projektzeitType.setTaetigkeit("reisen");
+        projektzeitType.setReiseRichtung("0");  // JourneyDirection.TO
+        final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm");
+
+        List<ProjectEntry> result = mapper.mapToEntryList(List.of(projektzeitType));
+
+        assertEquals(1, result.size());
+        final JourneyTimeEntry journeyTimeEntry = (JourneyTimeEntry) result.get(0);
+        assertEquals(projektzeitType.getReiseRichtung(), journeyTimeEntry.getJourneyDirection().getDirection());
+        assertEquals(projektzeitType.getDatum(), journeyTimeEntry.getDate().format(dateFormatter));
+        assertEquals(projektzeitType.getVon(), journeyTimeEntry.getFromTime().format(timeFormatter));
+        assertEquals(projektzeitType.getBis(), journeyTimeEntry.getToTime().format(timeFormatter));
+        assertEquals(projektzeitType.getTaetigkeit().toUpperCase(), journeyTimeEntry.getTask().name());
     }
 
     private static ProjektzeitType createDefaultProjektzeitType() {
