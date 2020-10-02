cd4964d1ba7d3075b19a8805643d9137eccb2f31
aslary <44347688+aslary@users.noreply.github.com>
2020-10-02 14:00:33 +0200

    renamings and PM View employee selection

--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/project-management/components/project-management.component.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/project-management/components/project-management.component.ts
@@ -4,6 +4,7 @@ import projectManagementResponseMock from '../models/MockData';
 import { MatDialog } from '@angular/material/dialog';
 import { CommentsForEmployeeComponent } from '../../shared/components/comments-for-employee/comments-for-employee.component';
 import { SelectionModel } from '@angular/cdk/collections';
+import { State } from '../../shared/models/State';
 
 @Component({
   selector: 'app-project-management',
@@ -11,8 +12,9 @@ import { SelectionModel } from '@angular/cdk/collections';
   styleUrls: ['./project-management.component.scss']
 })
 export class ProjectManagementComponent implements OnInit {
-  projectManagementResponse: Map<string, Array<ProjectManagementEntry>> = projectManagementResponseMock;
+  pmEntries: Map<string, Array<ProjectManagementEntry>> = projectManagementResponseMock;
   displayedColumns = [
+    'select',
     'employeeName',
     'projectCheckState',
     'employeeCheckState',
@@ -20,23 +22,47 @@ export class ProjectManagementComponent implements OnInit {
     'customerCheckState',
     'doneCommentsIndicator'
   ];
-  selection = new SelectionModel<ProjectManagementEntry>(true, []);
+  pmSelectionModels: Array<SelectionModel<ProjectManagementEntry>>;
 
   constructor(private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
+    if (this.pmEntries) {
+      this.pmSelectionModels = [];
+      for (let i = 0; i < this.pmEntries.size; i++) {
+        this.pmSelectionModels.push(new SelectionModel<ProjectManagementEntry>(true, []));
+      }
+    }
+  }
+
+  areAllSelected(projectIndex: number, projectKey: string) {
+    return this.pmSelectionModels[projectIndex].selected.length === this.pmEntries.get(projectKey).length;
+  }
+
+  masterToggle(projectIndex: number, projectKey: string) {
+    this.areAllSelected(projectIndex, projectKey) ?
+      this.pmSelectionModels[projectIndex].clear() :
+      this.pmEntries.get(projectKey).forEach(row => this.pmSelectionModels[projectIndex].select(row));
   }
 
   openDialog(pmEntry: ProjectManagementEntry): void {
     const dialogRef = this.dialog.open(CommentsForEmployeeComponent,
       {
         width: '100%',
-        autoFocus: false
+        autoFocus: false,
       }
     );
 
     dialogRef.componentInstance.employee = pmEntry.employeeName;
     dialogRef.componentInstance.comments = pmEntry.comments;
+  }
+
+  isAnySelected(): boolean {
+    return this.pmSelectionModels.filter(pmSelectionModel => pmSelectionModel.selected.length > 0).length > 0;
+  }
+
+  areAllProjectCheckStatesDone(projectKey: string): boolean {
+    return this.pmEntries.get(projectKey).every(pmEntry => pmEntry.projectCheckState === State.DONE);
   }
 }
