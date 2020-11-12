34087be4143c20b2c7bb754de84ee133625c9761
Rainer Neunteufel <rainer.neunteufel@gepardec.com>
2020-11-12 13:36:55 +0100

    fixed issue which results in step entries for users on not assigned projects, fixed issue on wrong filtering of internal projects

--- mega-zep-backend/src/main/java/com/gepardec/mega/service/impl/init/StepEntrySyncServiceImpl.java
+++ mega-zep-backend/src/main/java/com/gepardec/mega/service/impl/init/StepEntrySyncServiceImpl.java
@@ -51,10 +51,10 @@ public class StepEntrySyncServiceImpl implements StepEntrySyncService {
         final StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        logger.info("started generation at {}", Instant.ofEpochMilli(stopWatch.getStartTime()));
+        logger.info("Started step entry generation: {}", Instant.ofEpochMilli(stopWatch.getStartTime()));
 
         final LocalDate date = LocalDate.now().minusMonths(1).with(TemporalAdjusters.firstDayOfMonth());
-        logger.info("running step entry generation for {}", date);
+        logger.info("Processing date: {}", date);
 
         final List<User> activeUsers = userService.getActiveUsers();
         final List<Project> projectsForMonthYear = projectService.getProjectsForMonthYear(date, List.of(
@@ -70,10 +70,14 @@ public class StepEntrySyncServiceImpl implements StepEntrySyncService {
                 .map(Optional::get)
                 .collect(Collectors.toList());
 
-        logger.info("active users are {}", activeUsers);
-        logger.info("projects are {}", projectsForMonthYear);
-        logger.info("steps are {}", steps);
-        logger.info("omUsers are {}", omUsers);
+        logger.info("Loaded projects: {}", projectsForMonthYear.size());
+        logger.debug("projects are {}", projectsForMonthYear);
+        logger.info("Loaded users: {}", activeUsers.size());
+        logger.debug("Users are: {}", activeUsers);
+        logger.info("Loaded steps: {}", steps.size());
+        logger.debug("Steps are: {}", steps);
+        logger.info("Loaded omUsers: {}", omUsers.size());
+        logger.debug("omUsers are: {}", omUsers);
 
         final List<StepEntry> toBeCreatedStepEntries = new ArrayList<>();
 
@@ -91,8 +95,9 @@ public class StepEntrySyncServiceImpl implements StepEntrySyncService {
 
         stopWatch.stop();
 
-        logger.info("generation took {}ms", stopWatch.getTime());
-        logger.info("finished generation at {}", Instant.ofEpochMilli(stopWatch.getStartTime() + stopWatch.getTime()));
+        logger.info("Processed step entries: {}", toBeCreatedStepEntries.size());
+        logger.info("Step entry generation took: {}ms", stopWatch.getTime());
+        logger.info("Finished step entry generation: {}", Instant.ofEpochMilli(stopWatch.getStartTime() + stopWatch.getTime()));
     }
 
     private List<StepEntry> createStepEntriesProjectLeadForUsers(final LocalDate date, final Step step, final List<Project> projects, final List<User> users) {
@@ -104,6 +109,7 @@ public class StepEntrySyncServiceImpl implements StepEntrySyncService {
 
     private List<StepEntry> createStepEntriesForOwnerProjects(final LocalDate date, final Step step, final List<Project> projects, final List<User> users, final User owner) {
         return projects.stream()
+                .filter(project -> project.employees().contains(owner.userId()))
                 .map(project -> createStepEntriesForOwnerProject(date, step, project, users, owner))
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
