d55c9b13ff568c7a4441456118aba2063f261f0f
mgattringer <marko.gattringer@gepardec.com>
2020-12-03 10:59:55 +0100

    fixed wrong state in OM and PM view

--- mega-zep-backend/src/test/java/com/gepardec/mega/rest/ManagementResourceTest.java
+++ mega-zep-backend/src/test/java/com/gepardec/mega/rest/ManagementResourceTest.java
@@ -220,7 +220,7 @@ public class ManagementResourceTest {
         assertEquals(com.gepardec.mega.domain.model.State.DONE, entry.customerCheckState());
         assertEquals(com.gepardec.mega.domain.model.State.OPEN, entry.internalCheckState());
         assertEquals(com.gepardec.mega.domain.model.State.OPEN, entry.employeeCheckState());
-        assertEquals(com.gepardec.mega.domain.model.State.OPEN, entry.projectCheckState());
+        assertEquals(com.gepardec.mega.domain.model.State.DONE, entry.projectCheckState());
         assertEquals(mgattringer.email(), entry.employee().email());
         assertEquals(mgattringer.releaseDate(), entry.employee().releaseDate());
         assertEquals(3L, entry.totalComments());
@@ -304,7 +304,14 @@ public class ManagementResourceTest {
         stepEntry.setStep(createStep(stepName));
         stepEntry.setState(state);
         stepEntry.setDate(LocalDate.now());
+        stepEntry.setAssignee(createUser());
         return stepEntry;
+    }
+
+    private com.gepardec.mega.db.entity.User createUser() {
+        com.gepardec.mega.db.entity.User user = new com.gepardec.mega.db.entity.User();
+        user.setEmail("werner.bruckmueller@gpeardec.com");
+        return user;
     }
 
     private User createUserForRole(final Role role) {
