d1200cb71b9b709017f0605174d08446761ae32c
Philipp Wurm <philipp.wurm@gepardecom>
2021-06-23 18:40:37 +0200

    #356 changed loop to stream

--- mega-zep-backend/src/main/java/com/gepardec/mega/domain/model/ProjectState.java
+++ mega-zep-backend/src/main/java/com/gepardec/mega/domain/model/ProjectState.java
@@ -1,5 +1,7 @@
 package com.gepardec.mega.domain.model;
 
+import java.util.Arrays;
+
 public enum ProjectState {
     OPEN,
     WORK_IN_PROGRESS,
@@ -7,12 +9,8 @@ public enum ProjectState {
     NOT_RELEVANT;
 
     public static ProjectState byName(String name) {
-        for (ProjectState projectState : ProjectState.values()) {
-            if (projectState.name().equals(name)) {
-                return projectState;
-            }
-        }
-        throw new IllegalArgumentException(String.format("No project state for given name '%s' present.", name));
+        return Arrays.stream(ProjectState.values()).filter(v -> v.name().equalsIgnoreCase(name)).
+                findFirst().
+                orElseThrow(() -> new IllegalArgumentException(String.format("No project state for given name '%s' present.", name)));
     }
-
 }
