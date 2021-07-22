8b48f42e57f040c6a1a0ef97ca69e0ca397e9e4a
Michael Leitner <michael.leitner@gepardec.com>
2021-07-22 14:26:59 +0200

    #400 Implemented Component

---
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/monthly-report/components/general-info/genInfoData.ts
@@ -0,0 +1,9 @@
+export class genInfoData {
+  id: string;
+  value: string;
+
+  constructor(id: string, value:any) {
+    this.id = id;
+    this.value = value;
+  }
+}
--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/monthly-report/components/general-info/general-info.component.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/monthly-report/components/general-info/general-info.component.ts
@@ -1,36 +1,37 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {MonthlyReport} from '../../models/MonthlyReport';
-import {MatTableDataSource} from "@angular/material/table";
-import {Observable, of} from "rxjs";
+import {genInfoData} from "./genInfoData";
+
+const identifiers: string[] = [
+  'Arbeitszeit',
+  'fakturierbar',
+  'Urlaub',
+  'Zeitausgleich',
+  'Homeoffice'
+];
+
 
 @Component({
   selector: 'app-general-info',
   templateUrl: './general-info.component.html',
   styleUrls: ['./general-info.component.scss']
 })
+
+
 export class GeneralInfoComponent implements OnInit {
 
   @Input() monthlyReport: MonthlyReport;
   @Output() refreshMonthlyReport: EventEmitter<void> = new EventEmitter<void>();
 
-
-
-  monthlyReportDataSource: MatTableDataSource<MonthlyReport>;
-  //@ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  //@ViewChild(MatSort, { static: true }) sort: MatSort;
-
-
-  dataSource: string[];
-
-
+  dataSource: Array<genInfoData>;
   displayedColumns = ['identifier', 'values'];
 
   constructor() {
   }
 
-  ngOnInit(): void {
-    //let obs:Observable<MonthlyReport> = of(this.monthlyReport);
-
+  ngOnInit():
+    void {
+    this.dataSource = this.createDatasource(this.monthlyReport);
   }
 
 
@@ -38,7 +39,13 @@ export class GeneralInfoComponent implements OnInit {
     return [monthlyReport.totalWorkingTime, monthlyReport.billableTime, monthlyReport.vacationDays.toString(), monthlyReport.compensatoryDays.toString(), monthlyReport.homeofficeDays.toString()];
   }
 
-
-
+  createDatasource(monthlyReport: MonthlyReport): Array<genInfoData> {
+    let genInfoDataList: Array<genInfoData> = new Array<genInfoData>();
+    let monthlyReportData: string[] = this.extractData(monthlyReport);
+    for (let i = 0; i < monthlyReportData.length; i++) {
+      genInfoDataList.push(new genInfoData(identifiers[i], monthlyReportData[i]));
+    }
+    return genInfoDataList;
+  }
 
 }
