3939b4f65108bb0d72fdc57fce9a9fa229e1fa4f
Michael Leitner <michael.leitner@gepardec.com>
2021-07-22 12:48:13 +0200

    #400 Started Implementing general-info component

--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/monthly-report/components/general-info/general-info.component.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/monthly-report/components/general-info/general-info.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {MonthlyReport} from '../../models/MonthlyReport';
+import {MatTableDataSource} from "@angular/material/table";
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-general-info',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralInfoComponent implements OnInit {
 
-  constructor() { }
+  @Input() monthlyReport: MonthlyReport;
+  @Output() refreshMonthlyReport: EventEmitter<void> = new EventEmitter<void>();
+
+
+
+  monthlyReportDataSource: MatTableDataSource<MonthlyReport>;
+  //@ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  //@ViewChild(MatSort, { static: true }) sort: MatSort;
+
+
+  dataSource: string[];
+
+
+  displayedColumns = ['identifier', 'values'];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    //let obs:Observable<MonthlyReport> = of(this.monthlyReport);
+
   }
+
+
+  extractData(monthlyReport: MonthlyReport): string[] {
+    return [monthlyReport.totalWorkingTime, monthlyReport.billableTime, monthlyReport.vacationDays.toString(), monthlyReport.compensatoryDays.toString(), monthlyReport.homeofficeDays.toString()];
+  }
+
+
+
 
 }
