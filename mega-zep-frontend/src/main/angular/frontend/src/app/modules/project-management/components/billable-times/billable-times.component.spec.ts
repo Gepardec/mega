1776a758986554c5169340de1d9315ada6f514f4
Ollitod <olivertod11@yahoo.de>
2021-07-22 10:32:58 +0200

    #319 fix tests

--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/project-management/components/billable-times/billable-times.component.spec.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/project-management/components/billable-times/billable-times.component.spec.ts
@@ -1,4 +1,5 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import {BillableTimesComponent} from './billable-times.component';
 
@@ -8,7 +9,8 @@ describe('BillableTimesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [BillableTimesComponent]
+      declarations: [BillableTimesComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
       .compileComponents();
   }));
