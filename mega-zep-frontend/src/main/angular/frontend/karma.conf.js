c8668995fd80383a12ef4564f7807bb462ffb17c
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2021-11-23 09:07:24 +0100

    #33 added ChromeHeadlessNoSandbox to karma.config

--- mega-zep-frontend/src/main/angular/frontend/karma.conf.js
+++ mega-zep-frontend/src/main/angular/frontend/karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadlessNoSandbox'],
     autoWatch: false,
     restartOnFileChange: true
   });
