a35a11fb08dfca386215169760341b2402dfa244
Tod Oliver <olivertod11@yahoo.de>
2021-12-21 13:21:22 +0100

    #507 Fix mismatching month in monthly report on frontend

--- mega-zep-backend/src/main/java/com/gepardec/mega/rest/WorkerResource.java
+++ mega-zep-backend/src/main/java/com/gepardec/mega/rest/WorkerResource.java
@@ -15,6 +15,7 @@ import com.gepardec.mega.service.api.stepentry.StepEntryService;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -39,7 +40,7 @@ public class WorkerResource implements WorkerResourceAPI {
     @Override
     public MonthlyReport monthlyReport() {
         Employee employee = employeeService.getEmployee(Objects.requireNonNull(userContext.user()).userId());
-        LocalDate date = LocalDate.parse(employee.releaseDate());
+        LocalDate date = LocalDate.parse(employee.releaseDate()).with(TemporalAdjusters.firstDayOfNextMonth());
         return monthlyReport(date.getYear(), date.getMonthValue());
     }
 
