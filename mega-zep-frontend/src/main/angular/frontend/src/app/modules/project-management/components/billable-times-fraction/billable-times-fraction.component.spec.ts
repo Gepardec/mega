e3b3f9355ab6c63afde7273de5162f49fdffc35b
Patrick Holzer <patrick.holzer@gepardec.com>
2022-02-04 10:47:13 +0100

    #526 Test billable-times component

--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/project-management/components/billable-times-fraction/billable-times-fraction.component.spec.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/project-management/components/billable-times-fraction/billable-times-fraction.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import {BillableTimesFractionComponent} from './billable-times-fraction.component';
@@ -7,7 +7,7 @@ describe('BillableTimesFractionComponent', () => {
   let component: BillableTimesFractionComponent;
   let fixture: ComponentFixture<BillableTimesFractionComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [BillableTimesFractionComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -24,4 +24,9 @@ describe('BillableTimesFractionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  // TODO test billable and non-billable times are '00:00'
+
+  // TODO test billable and non-billable times are != '00:00'
 });
--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/project-management/components/billable-times/billable-times.component.spec.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/project-management/components/billable-times/billable-times.component.spec.ts
@@ -1,13 +1,15 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import {BillableTimesComponent} from './billable-times.component';
+import {By} from '@angular/platform-browser';
+import {expect} from "@angular/flex-layout/_private-utils/testing";
 
 describe('BillableTimesComponent', () => {
   let component: BillableTimesComponent;
   let fixture: ComponentFixture<BillableTimesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [BillableTimesComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -21,7 +23,48 @@ describe('BillableTimesComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display (non-)billable and transform them to fraction number', () => {
+    component.billableTimes = '80:30';
+    component.nonBillableTimes = '04:15';
+
+    fixture.detectChanges();
+
+    expect(billableTimesValue()).toEqual('80,50');
+    expect(nonBillableTimesValue()).toEqual('4,25');
+  });
+
+  it('should display horizontal rule if (non-)billable times are "00:00"', () => {
+    component.billableTimes = '00:00';
+    component.nonBillableTimes = '00:00';
+
+    fixture.detectChanges();
+
+    const billableTimesMatIcon = billableTimesElement().children[0];
+    expect(billableTimesMatIcon.name).toEqual("mat-icon");
+    expect(billableTimesMatIcon.nativeElement.innerHTML).toEqual("horizontal_rule");
+
+    const nonBillableTimesMatIcon = nonBillableTimesElement().children[0];
+    expect(nonBillableTimesMatIcon.name).toEqual("mat-icon");
+    expect(nonBillableTimesMatIcon.nativeElement.innerHTML).toEqual("horizontal_rule");
+  });
+
+
+  function billableTimesValue() {
+    return billableTimesElement().nativeElement.innerHTML;
+  }
+
+  function nonBillableTimesValue() {
+    return nonBillableTimesElement().nativeElement.innerHTML;
+  }
+
+  function billableTimesElement() {
+    return fixture.debugElement.query(By.css('.times-col-left'));
+  }
+  function nonBillableTimesElement() {
+    return fixture.debugElement.query(By.css('.times-col-right'));
+  }
 });
