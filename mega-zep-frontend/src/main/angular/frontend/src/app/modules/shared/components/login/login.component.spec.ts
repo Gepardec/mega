088c5462fdfaa9ee63daad01b640eb3a2e9136bc
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2022-02-07 12:47:04 +0100

    #526 finished LoginComponent test

--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/shared/components/login/login.component.spec.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/shared/components/login/login.component.spec.ts
@@ -1,18 +1,62 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {LoginComponent} from './login.component';
 import {OAuthService} from 'angular-oauth2-oidc';
 import {RouterTestingModule} from '@angular/router/testing';
 import {UserService} from '../../services/user/user.service';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {TranslateModule} from '@ngx-translate/core';
 
 describe('LoginComponent', () => {
+
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  class OAuthServiceMock {
+  let authService: OAuthService;
+  let userService: UserService;
 
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        {provide: OAuthService, useClass: OAuthServiceMock},
+        {provide: UserService, useClass: UserServiceMock}
+      ]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(LoginComponent);
+      component = fixture.componentInstance;
+
+      authService = TestBed.inject(OAuthService);
+      userService = TestBed.inject(UserService);
+    });
+  }));
+
+  it('#should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('#login - should call authService.initLoginFlow()', () => {
+    spyOn(authService, 'initLoginFlow').and.stub();
+
+    component.login();
+
+    expect(authService.initLoginFlow).toHaveBeenCalled();
+  });
+
+  it('#loggedIn - should call userService.loggedInWithGoogle and return true', () => {
+    spyOn(userService, 'loggedInWithGoogle').and.returnValue(true);
+
+    const loggedIn = component.loggedIn();
+
+    expect(userService.loggedInWithGoogle).toHaveBeenCalled();
+    expect(loggedIn).toBeTrue();
+  });
+
+  class OAuthServiceMock {
+    initLoginFlow() {
+    }
   }
 
   class UserServiceMock {
@@ -21,34 +65,4 @@ describe('LoginComponent', () => {
       return true;
     }
   }
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent],
-      imports: [
-        RouterTestingModule,
-        TranslateModule.forRoot()
-      ],
-      providers: [
-        {
-          provide: OAuthService, useClass: OAuthServiceMock
-        },
-        {
-          provide: UserService, useClass: UserServiceMock
-        }
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
