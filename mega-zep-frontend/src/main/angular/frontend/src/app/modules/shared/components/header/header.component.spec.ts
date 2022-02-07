f3a235aa7ddc2ba5df3770c534caf2acbcdcff9f
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2022-02-07 15:50:22 +0100

    #526 finished HeaderComponent test

--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/shared/components/header/header.component.spec.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/shared/components/header/header.component.spec.ts
@@ -1,32 +1,124 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {HeaderComponent} from './header.component';
 import {AppModule} from '../../../../app.module';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RolesService} from "../../services/roles/roles.service";
+import {UserService} from "../../services/user/user.service";
+import {TranslateService} from "@ngx-translate/core";
+import {BreakpointObserver} from "@angular/cdk/layout";
+import {ConfigService} from "../../services/config/config.service";
+import {expect} from "@angular/flex-layout/_private-utils/testing";
+import {of, Subscription} from "rxjs";
+import {Config} from "../../models/Config";
+import {Link} from "../../models/Link";
 
 describe('HeaderComponent', () => {
 
-  beforeEach(async(() => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  let rolesService: RolesService;
+  let userService: UserService;
+  let translate: TranslateService;
+  let breakpointObserver: BreakpointObserver;
+  let configService: ConfigService;
+
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         AppModule,
         HttpClientTestingModule
-      ],
-      declarations: [],
-      providers: []
+      ]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(HeaderComponent);
+      component = fixture.componentInstance;
+
+      rolesService = TestBed.inject(RolesService);
+      userService = TestBed.inject(UserService);
+      translate = TestBed.inject(TranslateService);
+      breakpointObserver = TestBed.inject(BreakpointObserver);
+      configService = TestBed.inject(ConfigService);
     });
   }));
 
-  function setup() {
-    const fixture: ComponentFixture<HeaderComponent> = TestBed.createComponent(HeaderComponent);
-    const app: HeaderComponent = fixture.debugElement.componentInstance;
+  it('#should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('#afterConstruction - should push 3 links', () => {
     fixture.detectChanges();
 
-    return {fixture, app};
+    expect(component.links.length).toEqual(3);
+  });
+
+  it('#afterInit - should call userService.user and configService.getConfig', () => {
+    fixture.detectChanges();
+
+    spyOn(configService, 'getConfig').and.returnValue(of(ConfigServiceMock.getConfig()));
+
+    component.ngOnInit();
+
+    expect(configService.getConfig).toHaveBeenCalled();
+  });
+
+  it('#afterDestroy', () => {
+    fixture.detectChanges();
+
+    spyOn(userService.user, 'subscribe').and.returnValue(new Subscription());
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect((component as any).userSubscription.closed).toBeFalse();
+    component.ngOnDestroy();
+
+    expect((component as any).userSubscription.closed).toBeTrue();
+  });
+
+  it('#showLink - should return isAllowed', () => {
+    fixture.detectChanges();
+
+    spyOn(rolesService, 'isAllowed').and.returnValue(true);
+
+    const isAllowed = component.showLink(LinkMock.getLink());
+
+    expect(rolesService.isAllowed).toHaveBeenCalled();
+    expect(isAllowed).toBeTrue();
+  });
+
+  it('#onLogout - should call userService.logout()', () => {
+    fixture.detectChanges();
+
+    spyOn(userService, 'logout').and.stub();
+
+    component.onLogout();
+
+    expect(userService.logout).toHaveBeenCalled();
+  });
+
+  class ConfigServiceMock {
+
+    static getConfig(): Config {
+      return {
+        clientId: 'DUMMY',
+        scope: 'email',
+        issuer: 'https://accounts.google.com',
+        version: '1',
+        excelUrl: 'DUMMY',
+        zepOrigin: 'DUMMY',
+        budgetCalculationExcelUrl: 'DUMMY'
+      };
+    }
   }
 
-  it('should create', () => {
-    const {fixture, app} = setup();
-    expect(app).toBeTruthy();
-  });
+  class LinkMock {
+
+    static getLink(): Link {
+      return {
+        name: "name",
+        path: "localhost:8080"
+      }
+    }
+  }
 });
