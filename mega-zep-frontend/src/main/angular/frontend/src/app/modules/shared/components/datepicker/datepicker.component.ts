63b2514d2b82a4cbf91ada10f45de5466c487fc9
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2022-02-09 14:46:55 +0100

    #526 finished DatepickerComponent Test

--- mega-zep-frontend/src/main/angular/frontend/src/app/modules/shared/components/datepicker/datepicker.component.ts
+++ mega-zep-frontend/src/main/angular/frontend/src/app/modules/shared/components/datepicker/datepicker.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {MatDatepicker, MatDatepickerInputEvent} from '@angular/material/datepicker';
 import {formatDate} from '@angular/common';
 import {configuration} from '../../constants/configuration';
@@ -8,25 +8,21 @@ import {configuration} from '../../constants/configuration';
   templateUrl: './datepicker.component.html',
   styleUrls: ['./datepicker.component.scss']
 })
-export class DatepickerComponent implements OnInit {
+export class DatepickerComponent {
+
+  @Input() selectedDate: string;
+  @Output() dateEmitter: EventEmitter<string> = new EventEmitter<string>();
+  @ViewChild('picker') datePicker: MatDatepicker<Date>;
 
   today: Date = new Date();
-  @ViewChild('picker') datePicker: MatDatepicker<Date>;
-  @Output() dateEmitter: EventEmitter<string> = new EventEmitter<string>();
-  @Input() selectedDate: string;
+
   private format = configuration.dateFormat;
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
 
   emitEvent(date: string): void {
     this.dateEmitter.emit(date);
   }
 
-  getDateAndEmitEvent(event: MatDatepickerInputEvent<Date>){
+  getDateAndEmitEvent(event: MatDatepickerInputEvent<Date>) {
     const date: string = formatDate(event.value, this.format, 'en-US');
     this.emitEvent(date);
   }
